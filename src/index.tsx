44584d25d9e01d7473a9bdf4091d5186178241cf
Pavel Sobolev <contact@paveloom.dev>
2024-11-10 16:01:20 +0300

    PWA: use the relative path to the `service-worker.js`.

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ if ("storage" in navigator && "persist" in navigator.storage) {
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/service-worker.js").then((registration) => {
+    navigator.serviceWorker.register("service-worker.js").then((registration) => {
       console.log("SW registered: ", registration);
     }).catch((registrationError) => {
       console.log("SW registration failed: ", registrationError);
