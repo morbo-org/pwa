e4608905967f59b9daa8a9ca6da28b768a26ebf3
Pavel Sobolev <contact@paveloom.dev>
2024-11-16 22:54:13 +0300

    PWA: style the `disabled` and `hover` states of buttons.

--- src/components/buttons/Button.vue
+++ src/components/buttons/Button.vue
@@ -9,16 +9,36 @@ defineEmits(["click"]);
 </template>
 
 <style>
+:root {
+  --color-button: var(--color-light-accent);
+  --color-button-outline: var(--color-light-icons-and-text);
+  --color-button-disabled: hsl(0deg 0% 75%);
+}
+
 button {
-    all: unset;
-    cursor: pointer;
-    background-color: var(--color-light-accent);
-    border-radius: 10px;
-    padding: 2px;
+  all: unset;
+  box-sizing: border-box;
+  cursor: pointer;
+  background-color: var(--color-button);
+  border-radius: 10px;
+  padding: 2px;
+}
+
+button.text {
+  padding: 0.5em;
 }
 
 button:focus-visible {
-    outline: var(--color-light-icons-and-text) solid 2px;
-    outline-offset: -1.5px;
+  outline: var(--color-button-outline) solid 2px;
+  outline-offset: -1.5px;
+}
+
+button:hover {
+  filter: brightness(0.9);
+}
+
+button:disabled {
+  background-color: var(--color-button-disabled);
+  cursor: not-allowed;
 }
 </style>
