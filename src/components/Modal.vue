b2bb4fdd5bce5e66fb98f8677ba93e76a13f61b9
Pavel Sobolev <contact@paveloom.dev>
2024-11-24 13:18:38 +0300

    PWA: export the `overlay` part of the `Modal` component.

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import Overlay from "@/components/Overlay.vue";
+
 const isOpen = defineModel<boolean>();
 
 function open() {
@@ -16,64 +18,7 @@ defineExpose({
 </script>
 
 <template>
-  <div v-if="isOpen" class="modal-overlay" @click.self="close">
-    <div class="modal-content">
-      <slot />
-    </div>
-  </div>
+  <Overlay v-if="isOpen" @click.self="close">
+    <slot />
+  </Overlay>
 </template>
-
-<style>
-.modal-overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 999;
-}
-
-.modal-content {
-  background-color: white;
-  padding: 20px;
-  border-radius: 8px;
-  max-width: 500px;
-  width: calc(90% - 40px);
-  max-height: calc(90vh - 40px);
-  overflow-y: auto;
-  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-  margin: 20px;
-}
-
-form {
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-}
-
-.form-input {
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-}
-
-.form-input > input {
-  box-sizing: border-box;
-  padding: 0.5rem;
-  height: 2rem;
-}
-
-.form-actions {
-  display: flex;
-  flex-direction: row;
-  gap: 0.5em;
-}
-
-.form-error {
-  color: red;
-}
-</style>
---
+++ src/components/Overlay.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+defineEmits(["self"]);
+</script>
+
+<template>
+  <div class="overlay" @click.self="$emit('self')">
+    <div class="overlay-content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style>
+.overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 999;
+}
+
+.overlay-content {
+  background-color: white;
+  padding: 20px;
+  border-radius: 8px;
+  max-width: 500px;
+  width: calc(90% - 40px);
+  max-height: calc(90vh - 40px);
+  overflow-y: auto;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+  margin: 20px;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+
+.form-input {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+}
+
+.form-input > input {
+  box-sizing: border-box;
+  padding: 0.5rem;
+  height: 2rem;
+}
+
+.form-actions {
+  display: flex;
+  flex-direction: row;
+  gap: 0.5em;
+}
+
+.form-error {
+  color: red;
+}
+</style>
